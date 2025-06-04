99144a06cb933a8c512f5f82d533301475eb01af
imad mohamed <abmohammedimad00@gmail.com>
2025-06-04 15:28:42 +0400

    Update Projects.jsx
    add live link

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -42,6 +42,15 @@ const Projects = () => {
               </h4>
               <p className='mt-2 text-slate-500'>{project.description}</p>
               <div className='mt-5 flex items-center gap-2 font-poppins'>
+
+                  <Link
+                  to={project.link}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className='font-semibold text-blue-600'
+                >
+                  Github
+                </Link>
                 <Link
                   to={project.link}
                   target='_blank'
