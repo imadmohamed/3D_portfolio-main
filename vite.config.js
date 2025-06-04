798ff458a6258c3724b5d5bd280228c7cbe4723c
imad mohamed <abmohammedimad00@gmail.com>
2025-06-04 17:03:24 +0400

    config at vite

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "3D_portfolio-main",
+
   assetsInclude: ['**/*.glb'],
 })
